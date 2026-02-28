04314a5f62a8757f316d2b332624ac06dc68409e
wendigothe48-maker <wendigothe48@gmail.com>
2026-02-28 23:22:09 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
-b
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Home route
+app.get("/", (req, res) => {
+    console.log("Hello");
+    res.send("Hello");
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
